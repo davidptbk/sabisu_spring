47d278b77e0c39ef20b75a2212ca3f92016e88a6
DavidPT <davidpt94@gmail.com>
2024-04-29 13:19:11 -0500

    Create project

---
+++ src/main/java/com/sabisu/Ecomerce/project/EComerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sabisu.Ecomerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabisu/Ecomerce/project/EComerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabisu.Ecomerce.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
