597542150152bc6fd4bf1490441945c083c767f2
Antony Martinez Ruiz <Antonymaruiz02@gmail.com>
2024-05-02 13:35:51 -0500

    entidad user

---
+++ src/main/java/com/riwi/sabisu/domain/entities/User.java
@@ -0,0 +1,20 @@
+package com.riwi.sabisu.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
